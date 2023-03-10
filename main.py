23422ddf4ef485d6739c79d6eaab3f89257d266b
kae1dy <58475910+kae1dy@users.noreply.github.com>
2023-03-10 19:25:06 +0300

    Delete main.py

--- main.py
+++
@@ -1,6 +0,0 @@
-from func import parse_code
-
-if __name__ == '__main__':
-    tokens = parse_code()
-    print(f"Example list of tokens: {tokens[0]}.\n"
-          f"Length of all array of arrays: {len(tokens)}.\n")
